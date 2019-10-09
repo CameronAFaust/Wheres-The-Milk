05efc748fe3bcdc78e72efee124ba5abda5b5af7
Cfaust <cfaust@student.neumont.edu>
2019-10-08 18:52:38 -0600

    Update / Delete items

--- screens/List.js
+++ screens/List.js
@@ -8,7 +8,8 @@ import {
   TouchableOpacity,
   View,
   TextInput,
-  Button
+  Button,
+  Modal
 } from "react-native";
 require("firebase/firestore");
 const firebase = require("firebase");
@@ -21,7 +22,9 @@ class HomeScreen extends Component {
       text: "",
       ItemList: [],
       userid: "",
-      modalVisible: false,
+      ModalVisibleStatus: false,
+      EditItem: "",
+      Updatedtext: ""
     };
   }
   componentDidMount() {
@@ -37,29 +40,19 @@ class HomeScreen extends Component {
     // Remove the event listener
     this.focusListener.remove();
   }
-
   _getInput() {
     this.setState({ ItemList: [...this.state.ItemList, this.state.text] });
     var userId = firebase.auth().currentUser.uid;
+    const update = {};
+    update[`${this.state.text}`] = this.state.text;
     const ref = firebase
       .firestore()
       .collection("users")
       .doc(userId)
       .collection("Lists")
-      .doc("List name");
-    firebase
-      .firestore()
-      .runTransaction(async transaction => {
-        const doc = await transaction.get(ref);
-        transaction.update(ref, {
-          list: this.state.ItemList
-        });
-      })
+      .doc("List name")
+      .set(update, { merge: true });
   }
-  _handleClick = pram => {
-    this.setState({modalVisible: true});
-  };
-
   _getList = () => {
     if (firebase.auth().currentUser != null) {
       var userId = firebase.auth().currentUser.uid;
@@ -73,8 +66,11 @@ class HomeScreen extends Component {
         .get()
         .then(function(doc) {
           if (doc.exists) {
+            usersList = [];
             user = doc.data();
-            usersList = user.list;
+            for (var key in user) {
+              usersList.push(user[key]);
+            }
           }
         })
         .catch(function(error) {
@@ -85,23 +81,53 @@ class HomeScreen extends Component {
         });
     }
   };
-
-  _editItem() {
-    console.log(this.state.text);
-    // this.setState({ ItemList: [...this.state.ItemList, this.state.text] })
-    console.log("updated");
+  _handleClick = pram => {
+    this.setState({ Updatedtext: pram.item });
+    this.setState({ EditItem: pram.item });
+    this.ShowModalFunction();
+  };
+  _editItem(original, Edited) {
+    const update = {};
+    if (firebase.auth().currentUser != null) {
+      var userId = firebase.auth().currentUser.uid;
+      update[`${original}`] = Edited;
+      var docRef = db
+        .collection("users")
+        .doc(userId)
+        .collection("Lists")
+        .doc("List name")
+        .update(update);
+    }
+    this.ShowModalFunction(false);
   }
-  _deleteItem() {
-    console.log(this.state.text);
-  }
+  _deleteItem(item) {
+    const update = {};
+    update[`${item}`] = firebase.firestore.FieldValue.delete();
+    var userId = firebase.auth().currentUser.uid;
 
+    const ref = firebase
+      .firestore()
+      .collection("users")
+      .doc(userId)
+      .collection("Lists")
+      .doc("List name")
+      .update(
+        update
+        // ,
+        // { merge: true }
+      );
+    // console.log(this.state.text);
+  }
   _handleListChange = pram => {
     this.setState({ ItemList: pram });
     console.log(this.state.ItemList);
   };
 
-  setModalVisible(visible) {
-    this.setState({ modalVisible: visible });
+  ShowModalFunction(visible) {
+    // this.state.Updatedtext
+    // this.setState({ Updatedtext: this.state.EditItem });
+    this.setState({ ModalVisibleStatus: visible });
+    console.log(this.state.Updatedtext);
   }
 
   render() {
@@ -131,7 +157,50 @@ class HomeScreen extends Component {
             </Button>
           ))}
         </View>
-        <View style={styles.container}></View>
+        <Modal
+          transparent={false}
+          animationType={"slide"}
+          visible={this.state.ModalVisibleStatus}
+          onRequestClose={() => {
+            this.ShowModalFunction(!this.state.ModalVisibleStatus);
+          }}
+        >
+          <View
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+          >
+            <View style={styles.ModalInsideView}>
+              <TextInput
+                style={styles.searchBar}
+                onChangeText={text => this.setState({ Updatedtext: text })}
+                value={this.state.Updatedtext}
+                clearTextOnFocus={false}
+              />
+              <Button
+                onPress={() =>
+                  this._editItem(this.state.EditItem, this.state.Updatedtext)
+                }
+                title="Update"
+                style={styles.ListItem}
+              >
+                Update
+              </Button>
+              <Button
+                onPress={() => this._deleteItem(this.state.EditItem)}
+                title="Delete"
+                style={styles.ListItem}
+              >
+                Delete
+              </Button>
+              <Button
+                title="Click Here To Hide Modal"
+                onPress={() => {
+                  this.ShowModalFunction(!this.state.ModalVisibleStatus);
+                }}
+              />
+            </View>
+          </View>
+        </Modal>
+        {/* <View style={styles.container}></View> */}
       </View>
     );
   }
@@ -168,6 +237,24 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignSelf: "center",
     marginTop: 10
+  },
+  ModalInsideView: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#00BCD4",
+    height: 300,
+    width: "90%",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#fff"
+  },
+
+  TextStyle: {
+    fontSize: 20,
+    marginBottom: 20,
+    color: "#fff",
+    padding: 20,
+    textAlign: "center"
   }
 });
 
